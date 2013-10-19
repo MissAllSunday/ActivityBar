5f263cf43748e22d35526dc30754f9661df225f5
Suki <suki@missallsunday.com>
2013-10-19 10:02:36 -0500

    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/languages/ActivityBar.english-utf8.php
+++ Themes/default/languages/ActivityBar.english-utf8.php
@@ -1,1 +1,13 @@
 <?php
+
+global $txt;
+
+$txt['Activity_Bar_title'] = 'Activity Bar';
+$txt['Activity_Bar_standardlabel'] = 'Activity';
+$txt['Activity_Bar_enable'] = 'Enable activity bar <div class="smalltext">Enable the activity bar, a bar that shows how active a user has been.</div>';
+$txt['Activity_Bar_label'] = 'Bar label <div class="smalltext">A label displayed along with the bar. Default is "'.$txt['Activity_Bar_standardlabel'].'".</div>';
+$txt['Activity_Bar_timeframe'] = 'Activity Timeframe <div class="smalltext">The modification will count all posts made during this timeframe. <br />In days. Default to 30.</div>';
+$txt['Activity_Bar_show_in_posts'] = 'Display the activity bar in posts';
+$txt['Activity_Bar_show_in_profile'] = 'Display the activity bar in profiles';
+$txt['Activity_Bar_max_posts'] = 'Max posts <div class="smalltext">Number of posts required in order to fill the bar. <br />Leave empty for defaults (500 posts).</div>';
+$txt['Activity_Bar_max_width'] = 'Max width  <div class="smalltext">The width of the bar. When the post limit set above is reached, the bar will have this width. <br />In pixels. Leave empty for defaults (139px).</div>';
--- Themes/default/languages/ActivityBar.spanish_latin-utf8.php
+++ Themes/default/languages/ActivityBar.spanish_latin-utf8.php
@@ -1,1 +1,23 @@
 <?php
+
+global $txt;
+
+<?php
+
+global $txt;
+
+$txt['Activity_Bar_title'] = 'Activity Bar';
+$txt['Activity_Bar_standardlabel'] = 'Actividad';
+$txt['Activity_Bar_enable'] = 'Activar la barra de actividad';
+$txt['Activity_Bar_enable_sub'] = 'Una barra que permite saber que tán activo a estado un usuario.';
+$txt['Activity_Bar_label'] = 'Etiqueta de la barra';
+$txt['Activity_Bar_label_sub'] = 'Una etiqueta que será mostrará junto con la barra. Default es: "'.$txt['Activity_Bar_title'] = 'Activity Bar';
+$txt['Activity_Bar_standardlabel'].'".';
+$txt['Activity_Bar_timeframe'] = 'Marco de tiempo';
+$txt['Activity_Bar_timeframe_sub'] = 'El mod contará todos los post hechos durante este tiempo. <br />En dias. Default es 30.';
+$txt['Activity_Bar_show_in_posts'] = 'Mostrar la barra de actividad en los temas.';
+$txt['Activity_Bar_show_in_profile'] = 'Mostrar la barra de actividad en los perfiles de usuarios.';
+$txt['Activity_Bar_max_posts'] = 'Máxima cantidad de mensajes';
+$txt['Activity_Bar_max_posts_sub'] = 'Es el númro de mensajes que se requieren para llebar la barra. <br />Déjalo vacio para usar el valor por default (500 mensajes).';
+$txt['Activity_Bar_max_width'] = 'Anchura máxima de la barra';
+$txt['Activity_Bar_max_width_sub'] = 'En pixels. Déjalo vacio para usar el valor por default (139px).';
