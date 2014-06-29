ca9a70e92089842933b93721791e8e6cd33ee7e6
Suki <suki@missallsunday.com>
2014-06-29 18:09:22 -0500

    Move the css file to Themes/css/ directory Make some nice UI changes to the bar
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/ActivityBar.php
+++ Sources/ActivityBar.php
@@ -89,16 +89,16 @@ class ActivityBar extends Ohara
 		// Get this user's activity.
 		$activity = $this->create($user);
 
-		// Declare some vars.
-		$placement = !empty($this->setting('placement')) ? $this->setting('placement') : 0;
-		$label = $this->setting('label') ? $this->setting('label') : $this->text('standardlabel');
+		// Append some vars.
+		$activity['placement'] = !empty($this->setting('placement')) && $this->setting('placement') != 0 ? (int) $this->setting('placement') : 0;
+		$activity['label'] = $this->setting('label') ? $this->setting('label') : $this->text('standardlabel');
 
 		// Append the data. Cheating, I'm gonna use a string key to make it easier for me to recognize this little buddy later...
 		$data['custom_fields']['Activity'] = array(
-			'title' => $label,
+			'title' => $activity['label'],
 			'col_name' => $this->setting('label') ? $this->setting('label') : $this->text('standardlabel'),
 			'value' => template_activity_display($activity),
-			'placement' => $placement,
+			'placement' => $activity['placement'],
 		);
 
 		unset($activity);
--- Themes/default/ActivityBar.template.php
+++ Themes/default/ActivityBar.template.php
@@ -36,19 +36,16 @@ function template_activity_profile($activity)
 {
 	global $txt, $modSettings;
 
-	if (empty($activity))
+	if (empty($activity) || !is_array($activity))
 		return false;
 
+	$width = !empty($modSettings['ActivityBar_max_width']) ? (int) $modSettings['ActivityBar_max_width'] : 139;
 	$template = '';
 
 	$template .='
-		<div class="activity_div">
-			<div class="activity_holder" style="width: '. (!empty($modSettings['ActivityBar_max_width']) ? $modSettings['ActivityBar_max_width'] : 139) .'px;">
-				<div class="ActivityBar" style="width: '. $activity['width'] .'px;">
-					<div class="activity_percentage">'. $activity['percentage'] .'%</div>
-				</div>
-			</div>
-		</div>';
+			<div class="activityBar blue stripes" style="width:'. $width .'px;" title="'. $activity['percentage'] .'%">
+				<span style="width: '. $activity['width'] .'%;" title="'. $activity['percentage'] .'%"></span>
+			</div>';
 
 	return $template;
 }
@@ -57,23 +54,19 @@ function template_activity_display($activity)
 {
 	global $txt, $modSettings;
 
-	if (empty($activity))
+	if (empty($activity) || !is_array($activity))
 		return false;
 
-	// Don't show the label if placement is "standard with title" as the code will do it automatically.
-	$label = $activity['placement'] ? $activity['title'] ? '';
-
+	// Don't show the label if the placement is "standard with title".
+	$label = !empty($activity['placement']) && $activity['placement'] != 0 ? $activity['label'] .':' : '';
+	$width = !empty($modSettings['ActivityBar_max_width']) ? (int) $modSettings['ActivityBar_max_width'] : 139;
 	$template = '';
 
 	$template .='
-		<div class="activity_div">
-			'. ($label) .'
-			<div class="activity_holder" style="width: '. (!empty($modSettings['ActivityBar_max_width']) ? $modSettings['ActivityBar_max_width'] : 139) .'px;">
-				<div class="ActivityBar" style="width: '. $activity['width'] .'px;">
-					<div class="activity_percentage">'. $activity['percentage'] .'%</div>
-				</div>
-			</div>
-		</div>';
+			'. $label .'
+			<div class="activityBar blue stripes" style="width:'. $width .'px;" title="'. $activity['percentage'] .'%">
+				<span style="width: '. $activity['width'] .'%;" title="'. $activity['percentage'] .'%"></span>
+			</div>';
 
 	return $template;
 }
